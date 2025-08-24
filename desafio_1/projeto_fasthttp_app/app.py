627cef1c63068d04179bdf86d017bf0ad5f074ee
Thiago88pe <thiago_alves88@hotmail.com>
2025-08-24 18:09:15 -0300

    feat: adiciona rota inicial com Hello World usando FastAPI

---
+++ desafio_1/projeto_fasthttp_app/app.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, host="0.0.0.0", port=8080)
